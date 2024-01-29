364908ecc8d65819c69d3830f1b5a66768078be9
raveena12345 <141730691+raveena12345@users.noreply.github.com>
2024-01-29 12:08:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,82 @@
+import { Fragment } from "react";
+// import { Switch, Route, Redirect, useLocation } from "react-router-dom";
+import { Route, Switch, Redirect, useLocation } from 'react-router-dom';
+
+import PrivateRoute from "./PrivateRoute";
+import Main from "./Pages/Main";
+import SignUp from "./Pages/SignUp";
+import Login from "./Pages/Login";
+import NotFound from "./Pages/NotFound";
+import Home from "./Pages/Home";
+import Library from "./Pages/Library";
+import Sidebar from "./Components/Sidebar";
+import Navbar from "./Components/Navbar";
+import AudioPlayer from "./Components/AudioPlayer";
+import Playlist from "./Pages/Playlist";
+import Search from "./Pages/Search";
+import LikedSongs from "./Pages/LikedSongs";
+import Profile from "./Pages/Profile";
+
+const App = () => {
+	const user = true;
+	const location = useLocation();
+
+	return (
+		<Fragment>
+			{user &&
+				location.pathname !== "/login" &&
+				location.pathname !== "/" &&
+				location.pathname !== "/signup" &&
+				location.pathname !== "/not-found" && (
+					<Fragment>
+						<Navbar />
+						<Sidebar />
+						<AudioPlayer />
+					</Fragment>
+				)}
+			<Switch>
+				<Route exact path="/" component={Main} />
+				<PrivateRoute exact user={user} path="/home" component={Home} />
+				<PrivateRoute
+					exact
+					user={user}
+					path="/collection/tracks"
+					component={LikedSongs}
+				/>
+				<PrivateRoute
+					exact
+					user={user}
+					path="/collection/playlists"
+					component={Library}
+				/>
+				<PrivateRoute exact user={user} path="/search" component={Search} />
+				<PrivateRoute
+					exact
+					user={user}
+					path="/playlist/:id"
+					component={Playlist}
+				/>
+				<PrivateRoute exact user={user} path="/me" component={Profile} />
+				{user && <Redirect from="/signup" to="/home" />}
+				{user && <Redirect from="/login" to="/home" />}
+				<Route path="/signup" component={SignUp} />
+				<Route path="/login" component={Login} />
+				<Route path="/not-found" component={NotFound} />
+				<Redirect to="/not-found" />
+			</Switch>
+		</Fragment>
+	);
+};
+
+export default App;
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       hello world
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import App from "./App";
+import "react-toastify/dist/ReactToastify.css";
+import "./global.css";
+
+ReactDOM.render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+			<ToastContainer
+				position="bottom-center"
+				autoClose={2000}
+				hideProgressBar={true}
+				closeButton={false}
+				theme="colored"
+				icon={false}
+			/>
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById("root")
+);
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './global.css';
+// import App from './App';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
